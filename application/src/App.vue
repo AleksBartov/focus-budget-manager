34adf472b61229cd1a0bc2a345372d7fc8d9adc2
Aleksandr <aleksandrbartov@yandex.ru>
2018-05-19 03:55:18 +0400

    we have cleaned vuejs generated files and created our own components such as Home.vue Authentication.vue

---
+++ application/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+@import "./assets/styles";
+
+  body {
+    background: url('./assets/images/church.jpeg') no-repeat center center fixed;
+    &:after {
+      content: '';
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-color: $background-tint;
+      opacity: .3;
+      z-index: -1;
+    }
+  }
+</style>
